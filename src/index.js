84cfcac580d03df34b44134fa78741e4194b0216
walter.wild@kakaocorp.com <walter.wild@kakaocorp.com>
2020-04-23 17:15:31 +0900

    https://www.howtographql.com/graphql-js/5-connecting-server-and-database/ workshop

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+
+// 1
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+  
+
+let idCount = links.length
+
+// 2  
+  const resolvers = {
+    Query: {
+      info: () => `This is the API of a Hackernews Clone`,
+      // 2
+      feed: (root, args, context, info) => {
+      return context.prisma.links()
+    },
+    },
+
+    Mutation: {
+        // 2
+        post: (parent, args) => {
+           const link = {
+            id: `link-${idCount++}`,
+            description: args.description,
+            url: args.url,
+          }
+          links.push(link)
+          return link
+        }
+      },
+  }
+
+// 3
+const server = new GraphQLServer({
+    typeDefs:'./src/schema.graphql',
+    resolvers,
+})
+
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
