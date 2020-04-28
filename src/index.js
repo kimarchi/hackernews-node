448538a7df29ff2d4046343bbacf62aacd371aa9
walter.wild@kakaocorp.com <walter.wild@kakaocorp.com>
2020-04-28 12:15:44 +0900

    chapter 6 https://www.howtographql.com/graphql-js/6-authentication/

--- src/index.js
+++ src/index.js
@@ -1,54 +1,30 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
-
-async function main() {
-
-  // Create a new link
-  const newLink = await prisma.createLink({ 
-    url: 'www.prisma.io',
-    description: 'Prisma replaces traditional ORMs',
-  })
-  console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`)
-
-  // Read all links from the database and print them to the console
-  const allLinks = await prisma.links()
-  console.log(allLinks)
-}
-
-main().catch(e => console.error(e))
-
-/*
 
 // 2  
   const resolvers = {
-    Query: {
-      info: () => `This is the API of a Hackernews Clone`,
-      // 2
-      feed: (root, args, context, info) => {
-        return context.prisma.links()
-      },
-    },
-
-    Mutation: {
-        // 2
-        post: (parent, args, context) => {
-          return context.prisma.createLink({
-            url: args.url,
-            description: args.description,
-          })
-        }
-      }, 
+    Query,
+    Mutation,
+    User,
+    Link, 
   }
 
 // 3
 const server = new GraphQLServer({
     typeDefs:'./src/schema.graphql',
     resolvers,
-    context: { prisma },
+    context: request => {
+      return {
+        ...request,
+        prisma,
+      }
+    },
 })
 
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
-
-*/
