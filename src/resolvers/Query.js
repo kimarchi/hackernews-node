3b94b06bca36d66f9737da591f3d5485645fcff7
walter.wild@kakaocorp.com <walter.wild@kakaocorp.com>
2020-04-28 15:33:19 +0900

    fixed query {info} error. insert the resolver

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,11 @@
 function feed(parent, args, context, info) {
     return context.prisma.links()
   }
-  
+
+function info(parent, args, context, info) {
+    return 'test'
+  }
   module.exports = {
     feed,
+    info,
   }
